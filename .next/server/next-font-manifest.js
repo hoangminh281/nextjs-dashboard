1a08dfecbd61df5d9f415c8787bafab40826a135
Minh Tran <minh.tran@mediastep.com>
2024-11-11 18:02:26 +0700

    Learning next chapters #2

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/mediastep/Documents/nextjs-learner/nextjs-dashboard/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\",\"static/media/ebd7dc65a6ba3e83-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
